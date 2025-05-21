3dfffdaf38af3e49f9fa769f82dd7548ef044c6b
Malika <emirmalika@gmail.com>
2025-05-22 00:18:50 +0500

    create a tableView with model Task

---
+++ ToDoList/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by Malik Em on 22.05.2025.
+//
+
+import Foundation
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView()
+        
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.delegate = self
+        table.dataSource = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+       
+        return table
+    }()
+    
+    var tasks: [String] = ["Купить хлеб", "Купить книгу", "Сходить в кино"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        navigationItem.title = "Задачи"
+        
+        setupLayout()
     }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
 
-
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
 }
 
